0b9a40e233f9d8c9c8dce7cb708a9f8daf14087d
JafKc <jaf222006@gmail.com>
2023-05-20 00:45:21 -0300

    Push to github

---
+++ src/backend.rs
@@ -0,0 +1,233 @@
+use serde_json::Value;
+use std::{
+    env::{self},
+    fs::{self, File},
+    io::{Read, Write},
+    path::Path,
+    process::Command,
+};
+
+pub(crate) mod version_installer;
+
+pub async fn start(
+    player: &str,
+    game_version: &str,
+    jvm: &str,
+    jvmargs: Vec<String>,
+    ram: f64,
+    gamemode: bool,
+    pdirectory: String,
+) {
+    let operationalsystem = std::env::consts::OS;
+    let player = player;
+    let mc_dir = match std::env::consts::OS {
+        "linux" => format!("{}/.minecraft", std::env::var("HOME").unwrap()),
+        "windows" => format!(
+            "{}/AppData/Roaming/.minecraft",
+            std::env::var("USERPROFILE").unwrap().replace("\\", "/")
+        ),
+        _ => panic!("System not supported."),
+    };
+
+    println!("{}", &mc_dir);
+    let assets_dir = format!("{}/assets", &mc_dir);
+    let game_version = game_version;
+    let uuid = "9791bffa968538928aa0b3ff397fd54f";
+
+    let versionpath = format!("{}/versions/{}/{}.jar", &mc_dir, game_version, game_version);
+
+    let jpathstring = format!(
+        "{}/versions/{}/{}.json",
+        &mc_dir, game_version, game_version
+    );
+    let jsonpath = Path::new(&jpathstring);
+
+    let mut file = File::open(jsonpath).unwrap();
+    let mut fcontent = String::new();
+    file.read_to_string(&mut fcontent).unwrap();
+    let content = serde_json::from_str(&fcontent);
+
+    let p: Value = content.unwrap();
+    let mainclass = &p["mainClass"].as_str().unwrap();
+    let mut assetindex = p["assets"].to_string();
+    let nativedir = format!("{}/versions/{}/natives", &mc_dir, game_version);
+
+    let mut libraries_list = libmanager(&p, operationalsystem, &mc_dir);
+
+    if game_version.contains("fabric-loader") {
+        let vanillaversion = p["inheritsFrom"].as_str().unwrap();
+        let vanillaversionpathstring = format!(
+            "{}/versions/{}/{}.jar",
+            &mc_dir, vanillaversion, vanillaversion
+        );
+        let vanillajsonpathstring = format!(
+            "{}/versions/{}/{}.json",
+            &mc_dir, vanillaversion, vanillaversion
+        );
+        let vanillajsonfilepath = Path::new(&vanillajsonpathstring);
+        if vanillajsonfilepath.exists() == false {
+            println!("{} needs to be installed.", vanillaversion);
+            version_installer::installversion(vanillaversion.to_string()).unwrap();
+
+            fs::remove_file(&versionpath).unwrap();
+            let mut vanillaversionfile = File::open(&vanillaversionpathstring).unwrap();
+            let mut buffer = Vec::new();
+            vanillaversionfile.read_to_end(&mut buffer).unwrap();
+            let mut fabric_towrite = File::create(&versionpath).unwrap();
+            fabric_towrite.write_all(&buffer).unwrap();
+        }
+
+        let mut vanillajson = File::open(&vanillajsonpathstring).unwrap();
+
+        let mut vjsoncontent = String::new();
+        vanillajson.read_to_string(&mut vjsoncontent).unwrap();
+        let vjson: Value = serde_json::from_str(&vjsoncontent).unwrap();
+        libraries_list.push_str(&libmanager(&vjson, operationalsystem, &mc_dir));
+        assetindex = vjson["assets"].to_string();
+    }
+    assetindex = assetindex.replace("\"", "");
+    libraries_list.push_str(&format!(
+        "{}/versions/{}/{}.jar",
+        &mc_dir, game_version, game_version
+    ));
+    if pdirectory.is_empty(){
+        env::set_current_dir(&pdirectory).expect("Failed to open Folder Profile!");
+    } else{
+    fs::create_dir_all(&pdirectory).unwrap();
+    env::set_current_dir(&mc_dir).expect("Failed to open Folder Profile!");
+    }
+    let mut mineprogram = if gamemode {
+        Command::new("gamemoderun")
+    } else {
+        Command::new(jvm)
+    };
+
+    if gamemode {
+        mineprogram.arg(jvm);
+    }
+    mineprogram
+        .arg(format!("-Xmx{}M", ram * 1024.))
+        .args(jvmargs)
+        .arg(format!("-Djava.library.path={}", nativedir))
+        .arg("-cp")
+        .arg(libraries_list)
+        .arg(mainclass)
+        .args([
+            "--username",
+            player,
+            "--version",
+            game_version,
+            "--accessToken",
+            "[pro]",
+            "--userProperties",
+            "{}",
+            "--gamedir",
+            &mc_dir,
+            "--assetsDir",
+            &assets_dir,
+            "--assetIndex",
+            &assetindex,
+            "--uuid",
+            uuid,
+            "--userType",
+            "legacy",
+        ]);
+    println!("{:?}", mineprogram);
+    mineprogram.spawn().expect("Failed to execute Minecraft!");
+}
+
+#[tokio::main]
+async fn libmanager(p: &Value, os: &str, mc_dir: &String) -> String {
+    let mut libraries_list = String::new();
+
+    if let Some(libraries) = p["libraries"].as_array() {
+        let lib_dir = format!("{}/libraries/", &mc_dir);
+        let separator = match os {
+            "linux" => ':',
+            "windows" => ';',
+            _ => panic!(),
+        };
+
+        for library in libraries {
+            if library["rules"][0]["os"]["name"] == os
+                || library["rules"][0]["os"]["name"].is_null()
+            {
+                let libraryname = library["name"].as_str().unwrap();
+                let mut lpieces: Vec<&str> = libraryname.split(":").collect();
+                let firstpiece = lpieces[0].replace(".", "/");
+                lpieces.remove(0);
+
+                if library["name"].as_str().unwrap().contains("natives") {
+                    lpieces.remove(lpieces.len() - 1);
+
+                    let libpath = format!(
+                        "{}{}/{}/{}-{}-natives-{}.jar",
+                        lib_dir,
+                        &firstpiece,
+                        &lpieces.join("/"),
+                        &lpieces[&lpieces.len() - 2],
+                        &lpieces[&lpieces.len() - 1],
+                        os
+                    );
+
+                    libraries_list.push_str(&libpath);
+                    libraries_list.push(separator);
+                } else if library["natives"][os].is_null() {
+                    let libpath = format!(
+                        "{}{}/{}/{}-{}.jar",
+                        lib_dir,
+                        &firstpiece,
+                        &lpieces.join("/"),
+                        &lpieces[&lpieces.len() - 2],
+                        &lpieces[&lpieces.len() - 1]
+                    );
+
+                    libraries_list.push_str(&libpath);
+                    libraries_list.push(separator);
+                } else {
+                    let libpath = format!(
+                        "{}{}/{}/{}-{}-natives-{}.jar",
+                        lib_dir,
+                        &firstpiece,
+                        &lpieces.join("/"),
+                        &lpieces[&lpieces.len() - 2],
+                        &lpieces[&lpieces.len() - 1],
+                        os
+                    );
+
+                    libraries_list.push_str(&libpath);
+                    libraries_list.push(separator);
+                }
+            }
+        }
+    }
+    return libraries_list;
+}
+
+pub fn getinstalledversions() -> Vec<String> {
+    let versions_dir = match std::env::consts::OS {
+        "linux" => format!("{}/.minecraft/versions", std::env::var("HOME").unwrap()),
+        "windows" => format!(
+            "{}/AppData/Roaming/.minecraft/versions",
+            std::env::var("USERPROFILE").unwrap().replace("\\", "/")
+        ),
+        _ => panic!("System not supported."),
+    };
+    if Path::new(&versions_dir).exists() == false {
+        fs::create_dir_all(&versions_dir).unwrap();
+    }
+    let entries = fs::read_dir(versions_dir).unwrap();
+
+    let versions = entries
+        .filter_map(|entry| {
+            let path = entry.unwrap().path();
+            if path.is_dir() {
+                Some(path.file_name().unwrap().to_string_lossy().to_string())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    versions
+}
---
+++ src/theme.rs
@@ -0,0 +1,465 @@
+use iced::overlay::menu;
+use iced::widget::{
+    button, container, pick_list, scrollable, slider, svg, text, text_input, toggler,
+};
+use iced::{application, color, Color};
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Theme;
+
+impl application::StyleSheet for Theme {
+    type Style = ();
+
+    fn appearance(&self, _style: &Self::Style) -> application::Appearance {
+        application::Appearance {
+            background_color: Color::from_rgb(
+                0.1098039215686275,
+                0.1098039215686275,
+                0.1529411764705882,
+            ),
+            text_color: color!(0xeb, 0xdb, 0xb2),
+        }
+    }
+}
+
+impl text::StyleSheet for Theme {
+    type Style = ();
+
+    fn appearance(&self, _style: Self::Style) -> text::Appearance {
+        text::Appearance {
+            color: color!(0xeb, 0xdb, 0xb2).into(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub enum Container {
+    #[default]
+    Default,
+}
+
+impl container::StyleSheet for Theme {
+    type Style = Container;
+
+    fn appearance(&self, _style: &Self::Style) -> container::Appearance {
+        container::Appearance::default()
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub enum Button {
+    #[default]
+    Primary,
+    Secondary,
+}
+
+impl button::StyleSheet for Theme {
+    type Style = Button;
+
+    fn active(&self, style: &Self::Style) -> button::Appearance {
+        match style {
+            Button::Primary => button::Appearance {
+                background: Color::from_rgb8(62, 123, 250).into(),
+                border_radius: 4.0,
+                border_width: 1.0,
+                border_color: color!(0x45, 0x85, 0x88),
+                ..Default::default()
+            },
+            Button::Secondary => button::Appearance {
+                background: Color::from_rgb8(5, 194, 112).into(),
+                ..Default::default()
+            },
+        }
+    }
+
+    fn hovered(&self, style: &Self::Style) -> button::Appearance {
+        match style {
+            Button::Primary => button::Appearance {
+                background: Color::from_rgb8(62, 123, 250).into(),
+                border_radius: 4.0,
+                border_width: 1.0,
+                border_color: Color::from_rgb(0.90, 0.90, 0.90),
+                ..Default::default()
+            },
+            Button::Secondary => button::Appearance {
+                background: Color::from_rgb8(5, 194, 112).into(),
+                border_radius: 4.0,
+                border_width: 1.0,
+                border_color: Color::from_rgb(0.90, 0.90, 0.90),
+                ..Default::default()
+            },
+        }
+    }
+}
+
+#[derive(Default)]
+pub enum TextInput {
+    #[default]
+    Default,
+}
+
+impl text_input::StyleSheet for Theme {
+    type Style = TextInput;
+
+    fn active(&self, _style: &Self::Style) -> text_input::Appearance {
+        text_input::Appearance {
+            background: iced::Background::Color(Color::from_rgb8(40, 41, 61)),
+            border_radius: 2.0,
+            border_width: 1.0,
+            border_color: Color::from_rgb8(40, 41, 61),
+            icon_color: Color::from_rgb(0.90, 0.90, 0.90),
+        }
+    }
+
+    fn hovered(&self, _style: &Self::Style) -> text_input::Appearance {
+        text_input::Appearance {
+            background: iced::Background::Color(Color::from_rgb8(40, 41, 61)),
+            border_radius: 2.0,
+            border_width: 1.0,
+            border_color: Color::from_rgb(0.90, 0.90, 0.90),
+            icon_color: Color::from_rgb(0.90, 0.90, 0.90),
+        }
+    }
+
+    fn focused(&self, _style: &Self::Style) -> text_input::Appearance {
+        text_input::Appearance {
+            background: iced::Background::Color(Color::from_rgb8(40, 41, 61)),
+            border_radius: 2.0,
+            border_width: 1.0,
+            border_color: Color::from_rgb(0.90, 0.90, 0.90),
+            icon_color: Color::from_rgb(0.90, 0.90, 0.90),
+        }
+    }
+
+    fn placeholder_color(&self, _style: &Self::Style) -> Color {
+        Color::from_rgb(0.90, 0.90, 0.90)
+    }
+
+    fn value_color(&self, _style: &Self::Style) -> Color {
+        Color::from_rgb(0.90, 0.90, 0.90)
+    }
+
+    fn selection_color(&self, _style: &Self::Style) -> Color {
+        Color::from_rgb(0.90, 0.90, 0.90)
+    }
+
+    fn disabled(&self, _style: &Self::Style) -> text_input::Appearance {
+        text_input::Appearance {
+            background: iced::Background::Color(Color::from_rgb(
+                0x20 as f32 / 255.0,
+                0x22 as f32 / 255.0,
+                0x25 as f32 / 255.0,
+            )),
+            border_radius: 2.0,
+            border_width: 1.0,
+            border_color: Color::from_rgb(
+                0x20 as f32 / 255.0,
+                0x22 as f32 / 255.0,
+                0x25 as f32 / 255.0,
+            ),
+            icon_color: Color::from_rgb(0.90, 0.90, 0.90),
+        }
+    }
+
+    fn disabled_color(&self, style: &Self::Style) -> Color {
+        self.placeholder_color(style)
+    }
+}
+
+#[derive(Clone, Default)]
+pub enum PickList {
+    #[default]
+    Default,
+}
+
+impl pick_list::StyleSheet for Theme {
+    type Style = PickList;
+
+    fn active(&self, style: &Self::Style) -> pick_list::Appearance {
+        match style {
+            PickList::Default => pick_list::Appearance {
+                text_color: Color::from_rgb(0.90, 0.90, 0.90),
+                background: iced::Background::Color(Color::from_rgb8(40, 41, 61)),
+                placeholder_color: Color::from_rgb(
+                    0x20 as f32 / 255.0,
+                    0x22 as f32 / 255.0,
+                    0x25 as f32 / 255.0,
+                ),
+                handle_color: Color::from_rgb(0.90, 0.90, 0.90),
+                border_radius: 2.0,
+                border_width: 1.0,
+                border_color: Color::from_rgb8(40, 41, 61),
+            },
+        }
+    }
+
+    fn hovered(&self, _style: &Self::Style) -> pick_list::Appearance {
+        pick_list::Appearance {
+            text_color: Color::from_rgb(0.90, 0.90, 0.90),
+            background: iced::Background::Color(Color::from_rgb8(40, 41, 61)),
+            placeholder_color: Color::from_rgb(
+                0x20 as f32 / 255.0,
+                0x22 as f32 / 255.0,
+                0x25 as f32 / 255.0,
+            ),
+            handle_color: Color::from_rgb(0.90, 0.90, 0.90),
+            border_radius: 2.0,
+            border_width: 1.0,
+            border_color: Color::from_rgb(0.90, 0.90, 0.90),
+        }
+    }
+}
+
+#[derive(Default)]
+pub enum Svg {
+    #[default]
+    Default,
+}
+
+impl svg::StyleSheet for Theme {
+    type Style = Svg;
+
+    fn appearance(&self, style: &Self::Style) -> svg::Appearance {
+        match style {
+            Svg::Default => Default::default(),
+        }
+    }
+}
+
+#[derive(Default)]
+pub enum Slider {
+    #[default]
+    Default,
+}
+
+impl slider::StyleSheet for Theme {
+    type Style = Slider;
+
+    fn active(&self, style: &Self::Style) -> slider::Appearance {
+        match style {
+            Slider::Default => {
+                let handle = slider::Handle {
+                    shape: slider::HandleShape::Rectangle {
+                        width: 8,
+                        border_radius: 4.0,
+                    },
+                    color: Color::from_rgb8(62, 123, 250),
+                    border_color: Color::from_rgb8(62, 123, 250),
+                    border_width: 1.0,
+                };
+
+                slider::Appearance {
+                    rail: slider::Rail {
+                        colors: (
+                            Color::from_rgb(0.90, 0.90, 0.90),
+                            Color::from_rgb(0.90, 0.90, 0.90),
+                        ),
+                        width: 2.0,
+                    },
+                    handle: slider::Handle {
+                        color: Color::from_rgb(
+                            0x20 as f32 / 255.0,
+                            0x22 as f32 / 255.0,
+                            0x25 as f32 / 255.0,
+                        ),
+                        border_color: Color::from_rgb(0.90, 0.90, 0.90),
+                        ..handle
+                    },
+                }
+            }
+        }
+    }
+
+    fn hovered(&self, style: &Self::Style) -> slider::Appearance {
+        match style {
+            Slider::Default => {
+                let active = self.active(style);
+
+                slider::Appearance {
+                    handle: slider::Handle {
+                        color: Color::from_rgb8(62, 123, 250),
+                        ..active.handle
+                    },
+                    ..active
+                }
+            }
+        }
+    }
+
+    fn dragging(&self, style: &Self::Style) -> slider::Appearance {
+        match style {
+            Slider::Default => {
+                let active = self.active(style);
+
+                slider::Appearance {
+                    handle: slider::Handle {
+                        color: Color::from_rgb8(62, 123, 250),
+                        ..active.handle
+                    },
+                    ..active
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone, Default)]
+pub enum Menu {
+    #[default]
+    Default,
+}
+
+impl menu::StyleSheet for Theme {
+    type Style = Menu;
+
+    fn appearance(&self, style: &Self::Style) -> menu::Appearance {
+        match style {
+            Menu::Default => menu::Appearance {
+                text_color: Color::from_rgb(0.90, 0.90, 0.90),
+                background: iced::Background::Color(Color::from_rgb8(40, 41, 61)),
+                border_width: 1.0,
+                border_radius: 0.0,
+                border_color: Color::from_rgb(0.90, 0.90, 0.90),
+                selected_text_color: Color::from_rgb(0.90, 0.90, 0.90),
+                selected_background: iced::Background::Color(Color::from_rgb8(62, 123, 250)),
+            },
+        }
+    }
+}
+
+impl From<PickList> for Menu {
+    fn from(_pick_list: PickList) -> Self {
+        Self::Default
+    }
+}
+
+#[derive(Default)]
+pub enum Scrollable {
+    #[default]
+    Default,
+}
+
+impl scrollable::StyleSheet for Theme {
+    type Style = Scrollable;
+
+    fn active(&self, style: &Self::Style) -> scrollable::Scrollbar {
+        match style {
+            Scrollable::Default => scrollable::Scrollbar {
+                background: Some(iced::Background::Color(Color::from_rgb(
+                    0x20 as f32 / 255.0,
+                    0x22 as f32 / 255.0,
+                    0x25 as f32 / 255.0,
+                ))),
+                border_radius: 2.0,
+                border_width: 0.0,
+                border_color: Color::TRANSPARENT,
+                scroller: scrollable::Scroller {
+                    color: Color::from_rgb(0.90, 0.90, 0.90),
+                    border_radius: 2.0,
+                    border_width: 0.0,
+                    border_color: Color::TRANSPARENT,
+                },
+            },
+        }
+    }
+
+    fn hovered(&self, style: &Self::Style, is_mouse_over_scrollbar: bool) -> scrollable::Scrollbar {
+        match style {
+            Scrollable::Default => {
+                if is_mouse_over_scrollbar {
+                    scrollable::Scrollbar {
+                        background: Some(iced::Background::Color(Color::from_rgb(
+                            0x20 as f32 / 255.0,
+                            0x22 as f32 / 255.0,
+                            0x25 as f32 / 255.0,
+                        ))),
+                        border_radius: 2.0,
+                        border_width: 0.0,
+                        border_color: Color::TRANSPARENT,
+                        scroller: scrollable::Scroller {
+                            color: Color::from_rgb(0.90, 0.90, 0.90),
+                            border_radius: 2.0,
+                            border_width: 0.0,
+                            border_color: Color::TRANSPARENT,
+                        },
+                    }
+                } else {
+                    self.active(style)
+                }
+            }
+        }
+    }
+
+    fn dragging(&self, style: &Self::Style) -> scrollable::Scrollbar {
+        match style {
+            Scrollable::Default => self.hovered(style, true),
+        }
+    }
+
+    fn active_horizontal(&self, style: &Self::Style) -> scrollable::Scrollbar {
+        match style {
+            Scrollable::Default => self.active(style),
+        }
+    }
+
+    fn hovered_horizontal(
+        &self,
+        style: &Self::Style,
+        is_mouse_over_scrollbar: bool,
+    ) -> scrollable::Scrollbar {
+        match style {
+            Scrollable::Default => self.hovered(style, is_mouse_over_scrollbar),
+        }
+    }
+
+    fn dragging_horizontal(&self, style: &Self::Style) -> scrollable::Scrollbar {
+        match style {
+            Scrollable::Default => self.hovered_horizontal(style, true),
+        }
+    }
+}
+
+#[derive(Default)]
+pub enum Toggler {
+    #[default]
+    Default,
+}
+
+impl toggler::StyleSheet for Theme {
+    type Style = Toggler;
+
+    fn active(&self, style: &Self::Style, is_active: bool) -> toggler::Appearance {
+        match style {
+            Toggler::Default => toggler::Appearance {
+                background: if is_active {
+                    Color::from_rgb8(62, 123, 250)
+                } else {
+                    Color::from_rgb(0.90, 0.90, 0.90)
+                },
+                background_border: None,
+                foreground: if is_active {
+                    Color::from_rgb(0.90, 0.90, 0.90)
+                } else {
+                    Color::from_rgb(0.5, 0.5, 0.5)
+                },
+                foreground_border: None,
+            },
+        }
+    }
+
+    fn hovered(&self, style: &Self::Style, is_active: bool) -> toggler::Appearance {
+        match style {
+            Toggler::Default => toggler::Appearance {
+                foreground: if is_active {
+                    Color {
+                        a: 0.5,
+                        ..Color::from_rgb(0.90, 0.90, 0.90)
+                    }
+                } else {
+                    Color::from_rgb(0.5, 0.5, 0.5)
+                },
+                ..self.active(style, is_active)
+            },
+        }
+    }
+}
