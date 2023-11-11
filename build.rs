3a25d1c9212b4de2d32899e5542a72d14a018add
JafKc <jaf222006@gmail.com>
2023-11-11 00:17:56 -0300

    Formatting

--- build.rs
+++ build.rs
@@ -1,12 +1,9 @@
 #[cfg(windows)]
 fn main() {
     let mut res = winres::WindowsResource::new();
-        res.set_icon("siglauncher.ico"); 
-        res.compile().unwrap();
+    res.set_icon("siglauncher.ico");
+    res.compile().unwrap();
 }
 
 #[cfg(unix)]
-fn main(){
-
-}
-
+fn main() {}
